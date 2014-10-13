55dc2518308025bcfc92dae4a1f7df876fae4058
Franco Ponticelli <franco.ponticelli@gmail.com>
2014-10-13 16:57:26 -0600

    release

--- doc/ImportPromise.hx
+++ doc/ImportPromise.hx
@@ -1,3 +1,5 @@
 import thx.promise.Future;
 import thx.promise.Promise;
+#if (java || js || flash)
 import thx.promise.Timer;
+#end
--- submit.sh
+++ submit.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 rm thx.promise.zip
-zip -r thx.promise.zip hxml src test extraParams.hxml haxelib.json LICENSE README.md
+zip -r thx.promise.zip hxml src test doc/ImportPromise.hx extraParams.hxml haxelib.json LICENSE README.md
 haxelib submit thx.promise.zip
