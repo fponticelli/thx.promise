ff5d9ebc16e484156aaa056a09abd4ee8d2717af
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-03-31 20:12:38 -0600

    exclude hidden files from zip

--- submit.sh
+++ submit.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 rm thx.promise.zip
-zip -r thx.promise.zip hxml src test doc/ImportPromise.hx extraParams.hxml haxelib.json LICENSE README.md
+zip -r thx.promise.zip hxml src test doc/ImportPromise.hx extraParams.hxml haxelib.json LICENSE README.md -x "*/\.*"
 haxelib submit thx.promise.zip
