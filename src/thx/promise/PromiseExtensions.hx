1a61d038b3df277ce016cbccc1e9fbfc0b7d0133
Andy White <greatwhitehope@gmail.com>
2016-07-13 15:58:40 -0600

    Second attempt at Promise<Option<T>> helpers

--- src/thx/promise/PromiseExtensions.hx
+++ src/thx/promise/PromiseExtensions.hx
@@ -4,6 +4,7 @@ import haxe.ds.Option;
 using thx.Arrays;
 import thx.Either;
 using thx.Eithers;
+import thx.Error;
 using thx.Options;
 
 class PromiseOptionExtensions {
@@ -11,9 +12,57 @@ class PromiseOptionExtensions {
 Converts an `Option<T>` into a `Promise<T>`.  `Some(value)` will result in a resolved promise of `value`,
 and `None` will result in a rejected promise with the given (optional) `errormessage`.
 **/
-  public static function toPromise<T>(option : Option<T>, ?errorMessage : String = "Optional value was None") : Promise<T> {
-    return option.cata(Promise.error(new Error(errorMessage)), Promise.value);
+  public static function toPromise<T>(option : Option<T>, error: Error) : Promise<T> {
+    return option.cata(Promise.error(error), Promise.value);
   }
+
+/**
+Mapping function for `Promise<Option<T>>` that allows you to intercept and handle a resolved value of `None` by
+returning a new value of type `T`.
+
+Similar to `Promise` `mapNull`
+
+If you need to convert `None` into an error rather than a new value of `T`, use `flatMapNone` or `mapNoneToError` instead.
+**/
+  public static function mapNone<T>(promise : Promise<Option<T>>, f : Void -> T) : Promise<T> {
+    return recoverNone(promise, function() {
+      return Promise.value(f());
+    });
+  }
+
+/**
+Map function for `Promise<Option<T>>` that converts a resolved value of `None` into a resolved `Promise<T>`
+of the given `value`.
+**/
+  public static function mapNoneToValue<T>(promise : Promise<Option<T>>, value : T) : Promise<T> {
+    return recoverNone(promise, function() {
+      return Promise.value(value);
+    });
+  }
+
+/**
+Map function for `Promise<Option<T>>` that converts a resolved value of `None` into a rejected `Promise<T>`
+of the given `Error`.
+**/
+  public static function mapNoneToError<T>(promise : Promise<Option<T>>, error : Error) : Promise<T> {
+    return recoverNone(promise, function() {
+      return Promise.error(error);
+    });
+  }
+
+/**
+Mapping function for `Promise<Option<T>>` that allows you to intercept and handle a resolved value of `None` by
+returning a new `Promise<T>` (success or failure).
+**/
+  public static function recoverNone<T>(promise : Promise<Option<T>>, f : Void -> Promise<T>) : Promise<T> {
+    return promise.flatMap(function(opt : Option<T>) {
+      return switch opt {
+        case Some(value) if (value != null) : Promise.value(value);
+        case _ : f();
+      };
+    });
+  }
+
 }
 
 class PromiseArrayExtensions {
--- test/thx/promise/TestPromiseExtensions.hx
+++ test/thx/promise/TestPromiseExtensions.hx
@@ -8,25 +8,105 @@ using thx.promise.PromiseExtensions;
 class TestPromiseExtensions {
   public function new() {}
 
-  public function testToPromiseSuccess() {
+  public function testOptionToPromise_Success() {
     var done = Assert.createAsync();
-    Some("hi").toPromise()
-      .success(function(value) {
-        Assert.same("hi", value); done();
-      })
-      .failure(function(e) {
-        Assert.fail(); done();
-      });
+    Some("hi").toPromise(new Error("no value"))
+      .success(function(value) { Assert.same("hi", value); done(); })
+      .failure(function(e) { Assert.fail(); done(); });
   }
 
-  public function testToPromiseFailure() {
+  public function testOptionToPromise_Failure() {
     var done = Assert.createAsync();
-    None.toPromise("this thing failed")
-      .success(function(value) {
-        Assert.fail(); done();
-      })
-      .failure(function(e) {
-        Assert.same("this thing failed", e.message); done();
-      });
+    None.toPromise(new Error("this thing failed"))
+      .success(function(value) { Assert.fail(); done(); })
+      .failure(function(e) { Assert.same("this thing failed", e.message); done(); });
+  }
+
+  public function testPromiseOption_MapNone_Some() {
+    var done = Assert.createAsync();
+    Promise.value(Some("hi"))
+      .mapNone(function() { return "bye"; })
+      .success(function(value) { Assert.same("hi", value); done(); })
+      .failure(function(e) { Assert.fail(); done(); });
+  }
+
+  public function testPromiseOption_MapNone_NoneToValue() {
+    var done = Assert.createAsync();
+    Promise.value(None)
+      .mapNone(function() { return "bye"; })
+      .success(function(value) { Assert.same("bye", value); done(); })
+      .failure(function(e) { Assert.fail(); done(); });
+  }
+
+  public function testPromiseOption_MapNone_NoneToException() {
+    var done = Assert.createAsync();
+    Promise.value(None)
+      .mapNone(function() { throw new Error("failed"); })
+      .success(function(value) { Assert.fail(); done(); })
+      .failure(function(e) { Assert.same("failed", e.message); done(); });
+  }
+
+  public function testPromiseOption_MapNoneToValue_Some() {
+    var done = Assert.createAsync();
+    Promise.value(Some("hi"))
+      .mapNoneToValue("bye")
+      .success(function(value) { Assert.same("hi", value); done(); })
+      .failure(function(e) { Assert.fail(); done(); });
+  }
+
+  public function testPromiseOption_MapNoneToValue_None() {
+    var done = Assert.createAsync();
+    Promise.value(None)
+      .mapNoneToValue("bye")
+      .success(function(value) { Assert.same("bye", value); done(); })
+      .failure(function(e) { Assert.fail(); done(); });
+  }
+
+  public function testPromiseOption_MapNoneToError_Some() {
+    var done = Assert.createAsync();
+    Promise.value(Some("hi"))
+      .mapNoneToError(new Error("failed"))
+      .success(function(value) { Assert.same("hi", value); done(); })
+      .failure(function(e) { Assert.fail(); done(); });
+  }
+
+  public function testPromiseOption_MapNoneToError_None() {
+    var done = Assert.createAsync();
+    Promise.value(None)
+      .mapNoneToError(new Error("failed"))
+      .success(function(value) { Assert.fail(); done(); })
+      .failure(function(e) { Assert.same("failed", e.message); done(); });
+  }
+
+  public function testPromiseOption_RecoverNone_Some() {
+    var done = Assert.createAsync();
+    Promise.value(Some("hi"))
+      .recoverNone(function() { return Promise.value("bye"); })
+      .success(function(value) { Assert.same("hi", value); done(); })
+      .failure(function(e) { Assert.fail(); done(); });
+  }
+
+  public function testPromiseOption_RecoverNone_NoneToResolved() {
+    var done = Assert.createAsync();
+    Promise.value(None)
+      .recoverNone(function() { return Promise.value("bye"); })
+      .success(function(value) { Assert.same("bye", value); done(); })
+      .failure(function(e) { Assert.fail(); done(); });
+  }
+
+  public function testPromiseOption_RecoverNone_NoneToRejected() {
+    var done = Assert.createAsync();
+    Promise.value(None)
+      .recoverNone(function() { return Promise.error(new Error("dead")); })
+      .success(function(value) { Assert.fail(); done(); })
+      .failure(function(e) { Assert.same("dead", e.message); done(); });
+  }
+
+  public function testPromiseOption_RecoverNone_NoneToException() {
+    var done = Assert.createAsync();
+    Promise.value(None)
+      .recoverNone(function() { throw new Error("dead"); })
+      .success(function(value) { Assert.fail(); done(); })
+      .failure(function(e) { Assert.same("dead", e.message); done(); });
   }
 }
