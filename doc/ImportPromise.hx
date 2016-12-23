8b0d206fbac3dd2a28b611d05ac820a76f822fb3
Franco Ponticelli <franco.ponticelli@gmail.com>
2016-12-23 09:55:33 -0700

    fix doc

--- doc/ImportPromise.hx
+++ doc/ImportPromise.hx
@@ -1,5 +1,8 @@
 import thx.promise.Future;
 import thx.promise.Promise;
+import thx.promise.PromiseExtensions;
+import thx.promise.PromiseR;
+import thx.promise.PromiseRExtensions;
 #if (js || flash)
 import thx.promise.Timer;
 #end
